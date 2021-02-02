4c17c6ef69091a17524c323a576d90f6ea9c0682
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-02-02 18:14:37 +0800

    fix doc path

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 (define collection "racket-project")
 (define deps '("base" "nanopass"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/racket-project.scrbl" ())))
+(define scribblings '(("scribblings/still-compiling.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
 (define pkg-authors '(dannypsnl))
