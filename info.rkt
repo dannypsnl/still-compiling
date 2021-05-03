0b8a5b8723d770b6aa17820b6cd3f4cf31155f20
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-05-04 01:54:05 +0800

    [ci] try fixing by lock version

--- info.rkt
+++ info.rkt
@@ -1,6 +1,7 @@
 #lang info
 (define collection "still-compiling")
-(define deps '("base" "nanopass"))
+(define deps '("base"
+               ["nanopass" #:version "6.2.1"]))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/still-compiling.scrbl" ())))
 (define pkg-desc "Description Here")
