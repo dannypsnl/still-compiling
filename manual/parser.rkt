26652f7a59bfff99504b0607343ee5b620934987
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-06-01 11:10:55 +0800

    parser: foundation

--- manual/parser.rkt
+++ manual/parser.rkt
@@ -2,16 +2,99 @@
 
 (require "lexer.rkt")
 
-(define (parse-binary l)
-  (define left (next l))
-  (define op (next l))
-  (define right (next l))
-  (must 'number left)
-  (must 'number right))
+; ast
+(struct expr () #:transparent)
+(struct binary expr (op left right) #:transparent)
 
-(define (must ty tok)
-  (unless (eq? (token-typ tok) ty)
-    (error 'must "~a but ~a" ty (token-typ tok))))
+; parse
+(define (parse-expr p left-hand-side previous-primary)
+  (define lhs (if left-hand-side
+                  left-hand-side
+                  (parse-unary p)))
 
-(define (next l)
-  (channel-get (lexer-items l)))
+  (let loop ([lookahead (peek p)])
+    (when (>= (precedence lookahead) previous-primary)
+      (define operator lookahead)
+      (take p)
+      (define rhs (parse-unary p))
+      (set! lookahead (peek p))
+      (let loop ()
+        (when (or (> (precedence lookahead) (precedence operator))
+                  (and (right-assoc? lookahead)
+                       (= (precedence lookahead) (precedence operator))))
+          (set! rhs (parse-expr p lhs (precedence lookahead)))
+          (set! lookahead (peek p))
+          (loop)))
+      (set! lhs (binary (token-typ operator)
+                        lhs rhs))
+      (loop lookahead)))
+
+  lhs)
+
+(define (right-assoc? token) #f)
+
+(define (parse-unary p)
+  (case (token-typ (peek p))
+    [(number) (take p)
+              (string->number (token-val (peek p)))]
+    [(true) (take p)
+            'true]
+    [(false) (take p)
+             'false]
+    [(identifier) (take p)
+                  (token-val (peek p))]
+    [else (error 'unknown "~a" (peek p))]))
+
+; helper
+(struct parser (name lexer tokens offset)
+  #:mutable
+  #:transparent)
+
+(define (parse name input)
+  (define lexer (lex name input))
+  (define p (parser name lexer (vector) 0))
+  (parse-expr p #f 1))
+
+(define (peek p [n 0])
+  (get-token p (+ (parser-offset p) n)))
+(define (take p)
+  (define origin (parser-offset p))
+  (set-parser-offset! p (add1 origin))
+  (get-token p origin))
+(define (consume p . wants)
+  (predict p wants))
+(define (predict p . wants)
+  (for ([i (length wants)]
+        [want wants])
+    (define tok (peek p i))
+    (unless (eq? (token-typ tok) want)
+      (error 'unexpected-token "want ~a, got ~a" want (token-typ tok)))))
+
+(define (get-token p fixed-offset)
+  (when (vector-empty? (parser-tokens p))
+    (get-token-from-lex p))
+  (define tokens (parser-tokens p))
+  (define last-token (vector-ref tokens (sub1 (vector-length tokens))))
+  (case (token-typ last-token)
+    [(EOF) last-token]
+    [else (get-token-from-lex p)
+          (set! tokens (parser-tokens p))])
+  (vector-ref tokens fixed-offset))
+(define (get-token-from-lex p)
+  (define l (parser-lexer p))
+  (define new-last-token (channel-get (lexer-items l)))
+  (set-parser-tokens! p
+                      (vector-append (parser-tokens p) (vector new-last-token))))
+
+(define (precedence token)
+  (case (token-typ token)
+    [(add sub) 2]
+    [(mul div) 3]
+    [else 0]))
+
+(module+ test
+  (require rackunit)
+
+  (check-equal? (parse "parsing" (open-input-string "12 + 23"))
+                (binary 'add 12 23)))
+
