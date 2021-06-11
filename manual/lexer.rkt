8d5c4fdf9436c64d80f839b2a137f512dee1bf69
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-06-11 09:01:34 +0800

    pos: add filename

--- manual/lexer.rkt
+++ manual/lexer.rkt
@@ -74,7 +74,7 @@
   lex-white-space)
 
 ; helpers
-(struct pos (line column) #:transparent)
+(struct pos (filename line column) #:transparent)
 
 (struct lexer
   (name
@@ -113,8 +113,8 @@
 (define (new-item l ty value)
   (channel-put (lexer-tokens l)
                (token ty value
-                      (pos (lexer-line l) (- (lexer-column l) (string-length value)))
-                      (pos (lexer-line l) (lexer-column l)))))
+                      (pos (lexer-name l) (lexer-line l) (- (lexer-column l) (string-length value)))
+                      (pos (lexer-name l) (lexer-line l) (lexer-column l)))))
 
 (define keyword=>keyword-type
   (make-hash
@@ -183,40 +183,43 @@
 (module+ test
   (require rackunit)
 
+  (define (test-pos l c)
+    (pos "test" l c))
+
   (test-case "lexing"
              (define l (lex "test" (open-input-string "31+12 abc")))
              (check-equal? (channel-get (lexer-tokens l))
-                           (token 'number "31" (pos 1 0) (pos 1 2)))
+                           (token 'number "31" (test-pos 1 0) (test-pos 1 2)))
              (check-equal? (channel-get (lexer-tokens l))
-                           (token 'add "+" (pos 1 2) (pos 1 3)))
+                           (token 'add "+" (test-pos 1 2) (test-pos 1 3)))
              (check-equal? (channel-get (lexer-tokens l))
-                           (token 'number "12" (pos 1 3) (pos 1 5)))
+                           (token 'number "12" (test-pos 1 3) (test-pos 1 5)))
              (check-equal? (channel-get (lexer-tokens l))
-                           (token 'identifier "abc" (pos 1 6) (pos 1 9))))
+                           (token 'identifier "abc" (test-pos 1 6) (test-pos 1 9))))
 
   (test-case "spacing"
              (define l (lex "test" (open-input-string "  abc")))
              (check-equal? (channel-get (lexer-tokens l))
-                           (token 'identifier "abc" (pos 1 2) (pos 1 5))))
+                           (token 'identifier "abc" (test-pos 1 2) (test-pos 1 5))))
 
   (test-case "keywords"
              (define l (lex "test" (open-input-string "true false")))
              (check-equal? (channel-get (lexer-tokens l))
-                           (token 'true "true" (pos 1 0) (pos 1 4)))
+                           (token 'true "true" (test-pos 1 0) (test-pos 1 4)))
              (check-equal? (channel-get (lexer-tokens l))
-                           (token 'false "false" (pos 1 5) (pos 1 10))))
+                           (token 'false "false" (test-pos 1 5) (test-pos 1 10))))
 
   (test-case "longer expression"
              (define l (lex "test" (open-input-string "1 + 2 * 3")))
              (check-equal? (channel-get (lexer-tokens l))
-                           (token 'number "1" (pos 1 0) (pos 1 1)))
+                           (token 'number "1" (test-pos 1 0) (test-pos 1 1)))
              (check-equal? (channel-get (lexer-tokens l))
-                           (token 'add "+" (pos 1 2) (pos 1 3)))
+                           (token 'add "+" (test-pos 1 2) (test-pos 1 3)))
              (check-equal? (channel-get (lexer-tokens l))
-                           (token 'number "2" (pos 1 4) (pos 1 5)))
+                           (token 'number "2" (test-pos 1 4) (test-pos 1 5)))
              (check-equal? (channel-get (lexer-tokens l))
-                           (token 'mul "*" (pos 1 6) (pos 1 7)))
+                           (token 'mul "*" (test-pos 1 6) (test-pos 1 7)))
              (check-equal? (channel-get (lexer-tokens l))
-                           (token 'number "3" (pos 1 8) (pos 1 9))))
+                           (token 'number "3" (test-pos 1 8) (test-pos 1 9))))
 
   )
--- manual/parser.rkt
+++ manual/parser.rkt
@@ -109,10 +109,12 @@
                 (binary 'add 12 (binary 'mul 23 34)))
 
   (test-case "increase token stream automatically"
+             (define (test-pos l c)
+               (pos "" l c))
              (define lexer (lex "" (open-input-string "12 + 23 * 34")))
              (define p (parser "" lexer (stream) 0))
              (check-equal? (get-token p 4)
-                           (token 'number "34" (pos 1 10) (pos 1 12))))
+                           (token 'number "34" (test-pos 1 10) (test-pos 1 12))))
 
   (test-case "right assoc"
              (check-equal? (parse "parsing" (open-input-string "12 ^ 23 ^ 34"))
