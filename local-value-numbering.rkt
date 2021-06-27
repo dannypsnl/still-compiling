8838aee1f7d4479f707865b3da25fd0b8f57377c
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-06-27 17:19:24 +0800

    extend local value numbering

--- local-value-numbering.rkt
+++ local-value-numbering.rkt
@@ -41,11 +41,62 @@
       `(,(map f inst*) ...)])
   (p prog))
 
-(define-parser parse IR)
-(local-value-numbering
- (parse '([a 1]
-          [b 2]
-          [c + a b]
-          [d - a b]
-          [e + a b]
-          [f e])))
+(define-pass extend-local-value-numbering : (IR Prog) (prog) -> (IR Prog) ()
+  (definitions
+    (define m (make-hash)))
+  (f : Inst (inst) -> Inst ()
+     [(,name ,expr)
+      (hash-set! m name (hash-ref m expr expr))
+      `(,name ,(hash-ref m expr expr))]
+     [(,name ,op ,expr0 ,expr1)
+      (define v0 (hash-ref m expr0))
+      (define v1 (hash-ref m expr1))
+      (define new-key (set op v0 v1))
+      (if (hash-ref m new-key #f)
+          (let ([v (hash-ref m new-key)])
+            (hash-set! m name v)
+            `(,name ,v))
+          (begin
+            (hash-set! m new-key name)
+            inst))])
+  (p : Prog (prog) -> Prog ()
+     [(,inst* ...)
+      `(,(map f inst*) ...)])
+  (p prog))
+
+(module+ test
+  (require rackunit)
+
+  (define-parser parse IR)
+
+  (check-equal? (local-value-numbering
+                 (parse '([a 1]
+                          [b 2]
+                          [c + a b]
+                          [d - a b]
+                          [e + a b]
+                          [f e]
+                          [g + b a])))
+                (parse '([a 1]
+                         [b 2]
+                         [c + a b]
+                         [d - a b]
+                         [e c]
+                         [f c]
+                         [g + b a])))
+
+  (check-equal? (extend-local-value-numbering
+                 (parse '([a 1]
+                          [b 2]
+                          [c + a b]
+                          [d - a b]
+                          [e + a b]
+                          [f e]
+                          [g + b a])))
+                (parse '([a 1]
+                         [b 2]
+                         [c + a b]
+                         [d - a b]
+                         [e c]
+                         [f c]
+                         [g c]))))
