28584306d5fb87131d224e1acba65e2f1ef417a8
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-12-23 10:43:04 +0800

    simplify inline-add
    * use `(+ ,n ...)` pattern so now we exclude all numbers case from
      general case

--- inline-add.rkt
+++ inline-add.rkt
@@ -12,18 +12,15 @@
 
 (define-pass inline-add : L (e) -> L ()
   (p : Expr (e) -> Expr ()
+     [(+ ,n ...) (apply + n)]
      [(+ ,[e] ...)
       (define number?-e (filter number? e))
-      (define non-number?-e (filter (compose not number?) e))
       (cond
-        [(and (> (length number?-e) 0)
-              (> (length non-number?-e) 0))
-         `(+ ,(apply + number?-e) ,non-number?-e ...)]
         [(> (length number?-e) 0)
-         `,(apply + number?-e)]
-        [else
-         `(+ ,e ...)])]))
+         `(+ ,(apply + number?-e) ,(filter (compose not number?) e) ...)]
+        [else `(+ ,e ...)])]))
 
 (define-parser parse L)
-(inline-add (parse '(+ (+ 1 2) a)))
-(inline-add (parse '(+ 2 a 3)))
+(define f (compose inline-add parse))
+(f '(+ (+ 1 2) a))
+(f '(+ 2 a 3))
