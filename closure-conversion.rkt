8d5c34f4521b7b2c7a593419e1414a32592e5bbe
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-02-18 19:06:06 +0800

    simplify closure-conversion
    * fix: use let binding to avoid reinvoke procedure

--- closure-conversion.rkt
+++ closure-conversion.rkt
@@ -11,6 +11,8 @@
         (vector e* ...)
         (begin body* ... body)
         (lambda (x* ...) body* ... body)
+        (let ([x* e*] ...)
+          body* ... body)
         (define (x x* ...)
           body* ... body)
         (define x e)
@@ -30,28 +32,28 @@
 (define-language L2
   (extends L1)
   (Expr (e body)
-        (- (lambda (x* ...) body* ... body))
-        (+ (lambda (x* ...) e))))
+        (- (lambda (x* ...) body* ... body)
+           (let ([x* e*] ...)
+             body* ... body))
+        (+ (lambda (x* ...) e)
+           (let ([x* e*] ...) e))))
 (define-pass begin-wrapping : L1 (e) -> L2 ()
   (Expr : Expr (e) -> Expr ()
         [(lambda (,x* ...) ,[body*] ... ,[body])
          (if (empty? body*)
              `(lambda (,x* ...) ,body)
              `(lambda (,x* ...)
+                (begin ,body* ... ,body)))]
+        [(let ([,x* ,e*] ...)
+           ,body* ... ,body)
+         (if (empty? body*)
+             `(let ([,x* ,e*] ...) ,body)
+             `(let ([,x* ,e*] ...)
                 (begin ,body* ... ,body)))]))
 
-(define-language L3
-  (extends L2)
-  (Expr (e body)
-        (+ (prim e e* ...))))
-(define-pass explicit-prim-call : L2 (e) -> L3 ()
-  (Expr : Expr (e) -> Expr ()
-        [(,[e] ,[e*] ...)
-         (if (member e '(+ - * / cons car cdr vector vector-ref))
-             `(prim ,e ,e* ...)
-             `(,e ,e* ...))]))
-
-(define-pass freevars : L3 (e) -> * ()
+(define (prim? e)
+  (member e '(+ - * / cons car cdr vector vector-ref)))
+(define-pass freevars : L2 (e) -> * ()
   (Expr : Expr (e) -> * ()
         [,x (set x)]
         [(lambda (,x* ...) ,e)
@@ -59,20 +61,19 @@
                        (list->set x*))]
         [(begin ,body* ... ,body)
          (apply set-union (map freevars (cons body body*)))]
-        [(,e ,e* ...) (apply set-union (map freevars (cons e e*)))]
-        [(prim ,e ,e* ...) (apply set-union (map freevars e*))]
+        [(,e ,e* ...)
+         (if (prim? e)
+             (apply set-union (map freevars e*))
+             (apply set-union (map freevars (cons e e*))))]
         [(define ,x ,e)
          (freevars e)]
         [else (set)]))
 
-(define-language L4
-  (extends L3))
-
-(define-pass replace-free : L4 (e $env fvs) -> L4 ()
+(define-pass replace-free : L2 (e $env fvs) -> L2 ()
   (Expr : Expr (e) -> Expr ()
         [,x (guard (set-member? fvs x))
-            `(prim vector-ref ,$env ,(index-of (set->list fvs) x))]))
-(define-pass closure-conversion : L3 (e) -> L4 ()
+            `(vector-ref ,$env ,(index-of (set->list fvs) x))]))
+(define-pass closure-conversion : L2 (e) -> L2 ()
   (Expr : Expr (e) -> Expr ()
         [(lambda (,x* ...) ,[body])
          (define $lifted-function-name (gensym 'lifted))
@@ -80,30 +81,27 @@
          (define fvs (freevars e))
          ; convert free-vars in body by using reference to $env
          (if (set-empty? fvs)
-             `(prim cons (lambda (,x* ... ,$env) ,body)
-                    (prim vector))
-             `(prim cons (lambda (,x* ... ,$env)
-                           ,(replace-free body $env fvs))
-                    (prim vector ,(set->list fvs) ...)))]))
+             `(cons (lambda (,x* ... ,$env) ,body)
+                    (vector))
+             `(cons (lambda (,x* ... ,$env)
+                      ,(replace-free body $env fvs))
+                    (vector ,(set->list fvs) ...)))]))
 
-(define-pass closure-call : L4 (e) -> L4 ()
+(define-pass closure-call : L2 (e) -> L2 ()
   (Expr : Expr (e) -> Expr ()
         [(,[e] ,[e*] ...)
-         `((prim car ,e) ,e* ... (prim cdr ,e))]))
-
-(define-pass unprim : L4 (e) -> L4 ()
-  (Expr : Expr (e) -> Expr ()
-        [(prim ,[e] ,[e*] ...)
-         `(,e ,e* ...)]))
+         (if (prim? e)
+             `(,e ,e* ...)
+             (let ([clos (gensym 'clos)])
+               `(let ([,clos ,e])
+                  ((car ,clos) ,e* ... (cdr ,clos)))))]))
 
 (define-parser parse-L0 L0)
-(define-parser parse-L4 L4)
+(define-parser parse-L2 L2)
 (define (all e)
-  ((compose unparse-L4
-            unprim
+  ((compose unparse-L2
             closure-call
             closure-conversion
-            explicit-prim-call
             begin-wrapping
             remove-define-lambda
             parse-L0)
@@ -116,6 +114,8 @@
          (+ m n)))
      ((make-adder 2) 3)))
 
-(all target)
+(displayln "gen code:")
+(pretty-display (all target))
 (define ev (make-evaluator 'racket))
+(displayln "result:")
 (ev (all target))
