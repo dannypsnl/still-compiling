76012ba0237b8669f9718764ce32243ba33aae2d
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-01-28 17:16:38 +0800

    In output is a pointer to write mode, `map` is a tail recursion
    misc: pretty print list content
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- polymorphic-runtime/main.c
+++ polymorphic-runtime/main.c
@@ -2,73 +2,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct _ListInt
-{
+typedef struct _ListInt {
   int first;
   struct _ListInt *second;
 } ListInt;
 
-void print_list_int(ListInt *l);
+void print_list_int(ListInt *l, bool first);
 
-bool empty(ListInt *l)
-{
-  if (l == NULL)
-  {
+bool empty(ListInt *l) {
+  if (l == NULL) {
     return true;
-  }
-  else
-  {
+  } else {
     return false;
   }
 }
 
-void single(int i, ListInt *out)
-{
+void single(int i, ListInt *out) {
   out->first = i;
   out->second = NULL;
 }
-void cons(int i, ListInt *l, ListInt *out)
-{
+void cons(int i, ListInt *l, ListInt *out) {
   out->first = i;
   out->second = l;
 }
 
-void map(int (*f)(int), ListInt *l, ListInt *out)
-{
-  if (l == NULL)
-  {
+void map(int (*f)(int), ListInt *l, ListInt *out) {
+again:
+  if (l == NULL) {
     out = NULL;
     return;
-  }
-  else
-  {
+  } else {
     out->first = f(l->first);
     out->second = l->second;
-    map(f, l->second, out->second);
-    return;
+
+    f = f;
+    l = l->second;
+    out = out->second;
+    goto again;
   }
 }
 
-int add1(int i)
-{
-  return i + 1;
-}
+int add1(int i) { return i + 1; }
 
-void print_list_int(ListInt *l)
-{
-  if (l != NULL)
-  {
-    printf("%d", l->first);
-    print_list_int(l->second);
-  }
-  else
-  {
+void print_list_int(ListInt *l, bool first) {
+  if (l != NULL) {
+    if (first) {
+      printf("%d", l->first);
+    } else {
+      printf(", %d", l->first);
+    }
+    print_list_int(l->second, false);
+  } else {
     printf("\n");
   }
 }
 
-int main()
-{
+int main() {
   ListInt *l = malloc(sizeof(ListInt));
   single(3, l);
   ListInt *l2 = malloc(sizeof(ListInt));
@@ -78,5 +67,5 @@ int main()
 
   ListInt *out = malloc(sizeof(ListInt));
   map(add1, l3, out);
-  print_list_int(out);
+  print_list_int(out, true);
 }
