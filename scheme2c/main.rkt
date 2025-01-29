3664ea04b9e08b9258aa1ce57f3ede34df2f9f2e
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-01-29 12:58:45 +0800

    more passes
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- scheme2c/main.rkt
+++ scheme2c/main.rkt
@@ -37,49 +37,83 @@
       (define binds
         (for/list ([x x*] [e e*])
           `(set! ,x ,e)))
-      `(begin
-        ,binds ...
-        ,body* ... ,body)]))
+      `(begin ,binds ...
+              ,body* ... ,body)]))
 
-(define-pass remove-complex-operands : L1 (e) -> L1 ()
+(define-language L2
+  (extends L1)
+  (Simple (s)
+    (+ n c b name
+      (s s* ...)))
+  (Expr (e body)
+    (- n
+       c
+       b
+       name)
+    (+ s)))
+(define-pass remove-complex-operands : L1 (e) -> L2 ()
+  (Atom : Expr (e) -> * ()
+    [,x (values x #f)]
+    [,n (values n #f)]
+    [,c (values c #f)]
+    [,b (values b #f)]
+    [else
+      (define new-var (gensym 'complex))
+      (define bind
+        (with-output-language (L2 Expr)
+          `(set! ,new-var ,(E e))))
+      (values new-var bind)])
   (E : Expr (e) -> Expr ()
-    [(set! ,x (begin ,[e*] ... ,[e]))
-      `(begin ,e* ... (set! ,x ,e))]
-    [(set! ,x (if ,[e0] ,[e1] ,[e2]))
-      `(if ,e0
-        (set! ,x ,e1)
-        (set! ,x ,e2))]
-  )
+    [(if ,e ,[e1] ,[e2])
+      (define-values (a bind) (Atom e))
+      (if bind
+        `(begin
+          ,bind
+          (if ,a ,e1 ,e2))
+        `(if ,a ,e1 ,e2))]
+    [(,e0 ,e* ...)
+      (define v (mutable-set))
+      (define-values (a a-binding) (Atom e0))
+      (set-add! v a-binding)
+      (define b*
+        (for/list ([e e*])
+          (define-values (b b-binding) (Atom e))
+          (set-add! v b-binding)
+          b))
+      `(begin
+        ,(set->list v) ...
+        (,a ,b* ...))])
   (E e))
 
 (define-language Final
-  (extends L1)
+  (extends L2)
   (Expr (e body)
     (+ (return e))))
-(define-pass auto : L1 (e) -> Final ()
-  (E : Expr (e) -> Expr ()))
-(define-pass wrap-return : L1 (e) -> Final ()
-  `(return ,(auto e)))
-
-(define-pass proper-return : Final (e) -> Final ()
+(define-pass wrap-return : L2 (e) -> Final ()
+  (E : Expr (e) -> Expr ())
+  `(return ,(E e)))
+(define-pass explicate-tail : Final (e) -> Final ()
   (E : Expr (e) -> Expr ()
     [(return (begin ,e* ... ,e))
-      `(begin ,e* ... (return ,e))]
+      `(begin ,e* ... ,(E e))]
     [(return (if ,e0 ,e1 ,e2))
-      `(if ,e0 (return ,e1) (return ,e2))]
-    [else e]))
+      `(if ,e0 ,(E e1) ,(E e2))])
+  (E e))
 
 (define used-variables (mutable-set))
 (define-pass 2c : Final (e) -> * ()
-  (E : Expr (e) -> * ()
-    [(return ,e)
-      (printf "return ")
-      (E e)
-      (printf ";")]
+  (S : Simple (s) -> * ()
     [,x (printf "~a" x)]
     [,n (printf "make_int(~a)" n)]
     [,c (printf "make_char('~a');" c)]
     [,b (if b (printf "make_bool(true)") (printf "make_bool(false)"))]
+    [else (error 'missing)])
+  (E : Expr (e) -> * ()
+    [,s (S s)]
+    [(return ,e)
+      (printf "return ")
+      (E e)
+      (printf ";")]
     [(begin ,e* ... ,e)
       (for-each E e*)
       (E e)]
@@ -88,15 +122,15 @@
       (printf "~a = " x)
       (E e)
       (printf ";\n")]
-    [(if ,e0 ,e1 ,e2)
+    [(if ,e ,e1 ,e2)
       (printf "if (to_bool(")
-      (E e0)
+      (E e)
       (printf ")) {")
       (E e1)
       (printf "} else {")
       (E e2)
       (printf "}")]
-    [else (void)])
+    [else (error 'missing "handling ~a" e)])
   (E e))
 
 (define (all-passes form)
@@ -106,19 +140,27 @@
   (parameterize ([current-output-port expr])
     ((compose
       2c
-      proper-return
+      explicate-tail
       wrap-return
       remove-complex-operands
       low-level-let
       parse-scm) form))
-  (printf "scm_t ")
+  (unless (set-empty? used-variables)
+    (printf "scm_t "))
   (for ([x (in-set used-variables)]
         [k (in-naturals)])
     (if (= k 0)
       (printf "~a" x)
       (printf ", ~a" x)))
-  (printf ";\n")
+  (unless (set-empty? used-variables)
+    (printf ";\n"))
+
   (printf (get-output-string expr))
+
   (printf "}\n"))
 
-(all-passes '(let ([x (let ([y 2]) y)]) x))
+(define out (open-output-file "tmp.c"
+  #:mode 'text
+  #:exists 'truncate/replace))
+(parameterize ([current-output-port out])
+  (all-passes '(if (let ([y #t]) y) 1 2)))
