407c69386fdb994e862cf4cd0a01ad81999c97c1
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-11-26 03:42:47 +0800

    update

--- usebindlib/bin/main.ml
+++ usebindlib/bin/main.ml
@@ -15,11 +15,12 @@ let rec eval : tm -> tm = function
   | t -> t
 ;;
 
-let mkfree : tm var -> tm = fun x -> Var x
+(* In this case mkfree is straightforward *)
+let mkfree (x : tm var) : tm = Var x
+let var' (name : string) : tm var = new_var mkfree name
 
-(* We need to provide a function that converts our term to boxed-term *)
-let rec box_term : tm -> tm box =
-  fun t ->
+(* We need to provide a function that wrap our term *)
+let rec box_term (t : tm) : tm box =
   match t with
   | Var x -> var x
   | Abs b ->
@@ -31,17 +32,14 @@ and var x =
   (* box_var is provide by bindlib *)
   box_var x
 
-and abs_raw : (tm, tm) binder box -> tm box = fun b -> box_apply (fun b -> Abs b) b
-and abs : tm var -> tm box -> tm box = fun x t -> abs_raw (bind_var x t)
+and abs_raw (b : (tm, tm) binder box) : tm box = box_apply (fun b -> Abs b) b
+and abs (x : tm var) (t : tm box) : tm box = abs_raw (bind_var x t)
 
-and app : tm box -> tm box -> tm box =
-  fun t u ->
-  (* box_apply2 is provided by bindlib, a functorial way to operate boxed-term *)
-  box_apply2 (fun t u -> App (t, u)) t u
-;;
+(* box_apply2 is provided by bindlib, a functorial way to operate boxed-term *)
+and app (t : tm box) (u : tm box) : tm box = box_apply2 (fun t u -> App (t, u)) t u
 
-let rec to_string : ctxt -> tm -> string =
-  fun ctxt t ->
+(* A recursive definition to print thing out *)
+let rec to_string (ctxt : ctxt) (t : tm) : string =
   match t with
   | Var x -> name_of x
   | Abs b ->
@@ -50,15 +48,39 @@ let rec to_string : ctxt -> tm -> string =
   | App (t, u) -> "(" ^ to_string ctxt t ^ ") " ^ to_string ctxt u
 ;;
 
-let to_string : tm -> string = fun t -> to_string (free_vars (box_term t)) t
+(* free_vars compute out a context for a boxed term *)
+let to_string (t : tm) : string = to_string (free_vars (box_term t)) t
 
+(* λx.x *)
 let id : tm =
-  let x = new_var mkfree "x" in
+  let x = var' "x" in
   unbox (abs x (var x))
 ;;
 
+(* λx.λy.x *)
+let fst : tm =
+  let x = var' "x" in
+  let y = var' "y" in
+  unbox (abs x (abs y (var x)))
+;;
+
+(* λx.(x) x) (boxed) *)
+let delta' : tm box =
+  let x = var' "x" in
+  abs x (app (var x) (var x))
+;;
+
+(* (λx.(x) x) λx.(x) x *)
+let omega : tm = unbox (app delta' delta')
+
+(* λx.(x) x) *)
+let delta : tm = unbox delta'
+
 let () =
   print_endline (to_string id);
-  let y = new_var mkfree "y" in
+  print_endline (to_string fst);
+  print_endline (to_string omega);
+  print_endline (to_string delta);
+  let y = var' "y" in
   print_endline (to_string (eval @@ unbox (app (box_term id) (var y))))
 ;;
