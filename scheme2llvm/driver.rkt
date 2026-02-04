b87f8a27657cc85f60eb8c41cea6bece3b114e39
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2026-02-04 11:21:31 +0800

    add flag -o for executable output, if not provided we use a.out

--- scheme2llvm/driver.rkt
+++ scheme2llvm/driver.rkt
@@ -3,18 +3,20 @@
          "compiler.rkt")
 
 (module+ main
-  (define debug-pass #f)
+  (define debug-pass (make-parameter #f))
+  (define output (make-parameter #f))
   (command-line
    #:program "ss"
    #:usage-help "usage: racket driver.rkt [--debug <pass>] <file>"
    #:once-each
    [("--debug") pass "Print result after pass (desugar, beta-reduce, constant-propagate, eliminate-deadcode, closure-conversion, closure-call, etc.)"
-                (set! debug-pass (string->symbol pass))]
+                (debug-pass (string->symbol pass))]
+   [("-o" "--output") out "Output executable"
+                      (output out)]
    #:args (input-file)
-   (when debug-pass
-     (debug-after debug-pass))
+   (when (debug-pass)
+     (debug-after (debug-pass)))
    (define bc-path (path-replace-extension input-file ".bc"))
-   (define out-path (path-replace-extension input-file ""))
    (define forms (with-input-from-file input-file
                    (lambda ()
                      (let loop ([acc '()])
@@ -23,6 +25,11 @@
                            (reverse acc)
                            (loop (cons e acc)))))))
    (compile-to-bitcode `(begin ,@forms) (path->string bc-path))
-   (define succ? (system* (find-executable-path "clang") (path->string bc-path) "runtime.o" "-o" (path->string out-path)))
+   (define succ?
+     (cond
+       [(output)
+        (system* (find-executable-path "clang") (path->string bc-path) "runtime.o" "-o" output)]
+       [else
+        (system* (find-executable-path "clang") (path->string bc-path) "runtime.o")]))
    (unless succ?
      (error 'compile "compile failed"))))
